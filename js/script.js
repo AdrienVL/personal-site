afff001044da2efa08ae0466f2e077b0a3f90ac5
AdrienVL <adrienlhemann@gmail.com>
2019-03-31 20:03:56 -0500

    Working on website

--- js/script.js
+++ js/script.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
             animation: "fade",
             play: 5000
 
-    })
+    });
+
+    var typed = new Typed(".typed", {
+
+        strings: ['software developer', 'World Traveler', 'Codeup Student'],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
 
 });
